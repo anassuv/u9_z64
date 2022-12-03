79ada7609d50bc3fed19d6d13b4c34c07e09dd1f
anassuv <114801727+anassuv@users.noreply.github.com>
2022-12-03 09:15:10 +0300

    all added

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z
+{
+    class Program
+    {
+        static void mrk(int n)
+        {
+            if (n < 1) return;
+            Console.Write("{0}, ", n);
+            mrk(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("\nN = {0} -> ", n);
+            mrk(n);
+
+            Console.ReadKey();
+        }
+    }
+}
